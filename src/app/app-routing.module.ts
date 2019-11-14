87e048d7568a8065aa59b510d885857ecd26dfde
Devailton <devailtonweb@gmail.com>
2019-11-14 18:48:26 -0300

    Pull na aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EventoComponent } from './evento/evento.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'evento', component: EventoComponent },
+	{ path: 'home', component: HomeComponent },
+	{ path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  	exports: [ RouterModule ],
+  	imports: [ RouterModule.forRoot(routes) ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventoComponent } from './evento/evento.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
